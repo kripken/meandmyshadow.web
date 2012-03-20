52f84076e0fcd4651a4b8cb7f9d4d577a07a6b0e
Alon Zakai <alonzakai@gmail.com>
2012-03-19 18:24:24 -0700

    strip debug info

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 # rm -Rf CMakeFiles/ CMakeCache.txt
 ~/Dev/emscripten/emconfigure cmake .
 ~/Dev/emscripten/emmake make
+~/Dev/llvm-emscripten/cbuild/bin/opt meandmyshadow -strip-debug -o=meandmyshadow.slim
 
