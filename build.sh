75e0341cc357f0d4eff8c3ce6f167ba80dbdf42d
Alon Zakai <alonzakai@gmail.com>
2012-03-19 18:31:16 -0700

    add build instruction to generate HTML. loads, fails on missing SDL mixer impls

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 # rm -Rf CMakeFiles/ CMakeCache.txt
 ~/Dev/emscripten/emconfigure cmake .
 ~/Dev/emscripten/emmake make
-~/Dev/llvm-emscripten/cbuild/bin/opt meandmyshadow -strip-debug -o=meandmyshadow.slim
+~/Dev/llvm-emscripten/cbuild/bin/opt meandmyshadow -strip-debug -o=meandmyshadow.bc
+~/Dev/emscripten/emcc meandmyshadow.bc -o mams.html
 
