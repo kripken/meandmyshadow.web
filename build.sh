0f2f8de792be3b7580e4e9c499fcb53ddf69bbfb
Alon Zakai <alonzakai@gmail.com>
2012-03-21 13:39:51 -0700

    build faster

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # rm -Rf CMakeFiles/ CMakeCache.txt
 ~/Dev/emscripten/emconfigure cmake .
-~/Dev/emscripten/emmake make VERBOSE=1
+~/Dev/emscripten/emmake make VERBOSE=1 -j 4
 mv meandmyshadow meandmyshadow.bc
 ~/Dev/emscripten/emcc meandmyshadow.bc -o mams.html
 
