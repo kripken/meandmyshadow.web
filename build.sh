161548b237ba1769c74f73fc3d3114dc8371dfc2
Alon Zakai <alonzakai@gmail.com>
2012-03-25 14:06:33 -0700

    disable llvm lto due to possible bug that needs investigating

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 #~/Dev/emscripten/emconfigure cmake .
 #~/Dev/emscripten/emmake make VERBOSE=1 -j 1
 #mv meandmyshadow meandmyshadow.bc
-~/Dev/emscripten/emcc meandmyshadow.bc -O2 --closure 0 -o mams.html --preload-file data --pre-js pre.js -s TOTAL_MEMORY=20971520
+~/Dev/emscripten/emcc meandmyshadow.bc -O2 --closure 0 -o mams.html --preload-file data --pre-js pre.js -s TOTAL_MEMORY=20971520 --llvm-lto 0
 
