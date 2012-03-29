13ca694b9baf38e77e176c0fbd2bf774e9686a0e
Alon Zakai <alonzakai@gmail.com>
2012-03-29 12:13:02 -0700

    build update

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 #~/Dev/emscripten/emconfigure cmake .
 #~/Dev/emscripten/emmake make VERBOSE=1 -j 1
 #mv meandmyshadow meandmyshadow.bc
-~/Dev/emscripten/emcc meandmyshadow.bc -O2 -o mams.html --preload-file data --pre-js pre.js -s TOTAL_MEMORY=20971520 --llvm-lto 0 --compression /home/alon/Dev/emscripten/third_party/lzma.js/lzma-native,/home/alon/Dev/emscripten/third_party/lzma.js/lzma-decoder.js,LZMA.decompress
+~/Dev/emscripten/emcc meandmyshadow.bc -O2 -o mams.html --preload-file data --pre-js pre.js -s TOTAL_MEMORY=20971520 --compression /home/alon/Dev/emscripten/third_party/lzma.js/lzma-native,/home/alon/Dev/emscripten/third_party/lzma.js/lzma-decoder.js,LZMA.decompress -s "EXPORTED_FUNCTIONS=['_main', '_SDL_Quit']"
+#~/Dev/emscripten/emcc meandmyshadow.bc -O2 -o mams.html --preload-file data --pre-js pre.js -s TOTAL_MEMORY=20971520 --minify 0
 
