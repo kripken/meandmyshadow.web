e86e126fdfd0a86f7922117db3acc249f84205cc
Alon Zakai <alonzakai@gmail.com>
2012-10-03 11:55:24 -0700

    build system improvements, and remove compression

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
-#rm -Rf CMakeFiles/ CMakeCache.txt
-#~/Dev/emscripten/emconfigure cmake .
-#~/Dev/emscripten/emmake make VERBOSE=1 -j 1
-#mv meandmyshadow meandmyshadow.bc
-~/Dev/emscripten/emcc meandmyshadow.bc -O2 -o mams.html --preload-file data --pre-js pre.js -s TOTAL_MEMORY=20971520 --compression /home/alon/Dev/emscripten/third_party/lzma.js/lzma-native,/home/alon/Dev/emscripten/third_party/lzma.js/lzma-decoder.js,LZMA.decompress -s "EXPORTED_FUNCTIONS=['_main', '_SDL_Quit']"
-#~/Dev/emscripten/emcc meandmyshadow.bc -O2 -o mams.html --preload-file data --pre-js pre.js -s TOTAL_MEMORY=20971520 --minify 0
+rm -Rf CMakeFiles/ CMakeCache.txt
+~/Dev/emscripten/emconfigure cmake .
+~/Dev/emscripten/emmake make VERBOSE=1 -j 4
+mv meandmyshadow meandmyshadow.bc
+~/Dev/emscripten/emcc meandmyshadow.bc -O2 -o mams.html --preload-file data --pre-js pre.js -s TOTAL_MEMORY=20971520 -s "EXPORTED_FUNCTIONS=['_main', '_SDL_Quit']"
 
